cd4e23d23f7d127b7a396b6b1e43cb605695c8c7
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-10-15 21:20:13 +0900

    削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,6 +11,11 @@ class ItemsController < ApplicationController
     Item.create(item_params)
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :text, :image, :category_id, :item_id)
