a754956ae582c48aacb2ad72d2b0f0e02ac327b1
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-10-15 17:31:51 +0900

    createアクションのveiwsまで完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,16 @@ class ItemsController < ApplicationController
   def new
     @items = Item.new
   end
-  
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :text, :image, :category_id, :item_id)
+  end
+
 end
 
 
