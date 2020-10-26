ca2166f0b837185b6cd5bef1724c694cff45c4da
uraokayutaro1990 <69355356+uraokayutaro1990@users.noreply.github.com>
2020-10-26 23:29:35 +0900

    Gemファイルを変更

--- Gemfile
+++ Gemfile
@@ -52,9 +52,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-<<<<<<< Updated upstream
-=======
 gem 'pry-rails'
 gem 'devise'
 gem 'active_hash'
->>>>>>> Stashed changes
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-=======
   devise_for :users
   root to: 'items#index'
   resources :items
@@ -10,6 +8,4 @@ end
   collection do
     get 'search'
   end
-end 
 
->>>>>>> Stashed changes
